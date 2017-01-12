d990fca981cc4a58daeb40c98760488968e2e9ad
eduardodeveloper <eduardotecnologo@hotmail.com>
2017-01-12 21:04:18 -0200

    Lista de Tarefas App1

--- Angular2Webpack/app1/app/app-component.js
+++ Angular2Webpack/app1/app/app-component.js
@@ -18,11 +18,21 @@ System.register(['angular2/core'], function(exports_1) {
         execute: function() {
             AppComponent = (function () {
                 function AppComponent() {
+                    this.tarefas = [];
                 }
+                AppComponent.prototype.adiconar = function () {
+                    if (this.txtTarefa != '') {
+                        this.tarefas.push(this.txtTarefa);
+                        this.txtTarefa = '';
+                    }
+                };
+                AppComponent.prototype.remover = function (index) {
+                    this.tarefas.splice(index, 1);
+                };
                 AppComponent = __decorate([
                     core_1.Component({
                         selector: 'meu-app',
-                        template: '<h1>Testando app com Angular2</h1>'
+                        template: "\n    <h1>Lista de tarefas com Angular2</h1>\n    <h2>Lista de tarefas</h2>\n    <ul>\n        <li *ngFor=\"#tarefa of tarefas; #i = index\">\n          {{tarefa}} <button (click)=\"remover(i)\">Excluir</button>\n        </li>\n    </ul>\n    <input type=\"text\" name=\"tarefa\" placeholder=\"Adicionar tarefas\" [(ngModel)]=\"txtTarefa\">\n    <button (click)=\"adiconar()\">Adicionar</button>\n  "
                     }), 
                     __metadata('design:paramtypes', [])
                 ], AppComponent);
--- Angular2Webpack/app1/app/app-component.ts
+++ Angular2Webpack/app1/app/app-component.ts
@@ -2,6 +2,31 @@ import {Component} from 'angular2/core';
 
 @Component({
   selector: 'meu-app',
-  template: '<h1>Testando app com Angular2</h1>'
+  template: `
+    <h1>Lista de tarefas com Angular2</h1>
+    <h2>Lista de tarefas</h2>
+    <ul>
+        <li *ngFor="#tarefa of tarefas; #i = index">
+          {{tarefa}} <button (click)="remover(i)">Excluir</button>
+        </li>
+    </ul>
+    <input type="text" name="tarefa" placeholder="Adicionar tarefas" [(ngModel)]="txtTarefa">
+    <button (click)="adiconar()">Adicionar</button>
+  `
 })
-export class AppComponent{}
+export class AppComponent{
+  public tarefas: string[] = [];
+
+  public txtTarefa: string;
+
+  adiconar() {
+    if(this.txtTarefa != '') {
+      this.tarefas.push(this.txtTarefa);
+      this.txtTarefa = '';
+    }
+  }
+
+  remover(index:number) {
+    this.tarefas.splice(index, 1);
+  }
+}
